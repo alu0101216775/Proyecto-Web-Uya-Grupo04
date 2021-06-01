4ca004562175a5120550dabfbffbe48fbc37b22c
alu0101216775 <alu0101216775@ull.edu.es>
2021-06-01 17:37:33 +0100

    Schedule funcionando

--- js/main.js
+++ js/main.js
@@ -1,52 +1,71 @@
-/**
- * Define una función en javascript en la que se inserte un texto en una tarjeta que el usuario introduce por el teclado
- */
-// $(document).ready(function () {
-//     $("#boton-cambio-texto").click(function () {
-//         $("#texto-usuario").text($("#texto-input").val());
-//     });
-// });
+//$(".button-collapse").sideNav();
 
-// Or with jQuery
-
-// $(document).ready(function () {
-//     $('.sidenav').sidenav();
-// });
-
-
-$(".button-collapse").sideNav();
-
-/**
- * Define una función javascript que calcule la edad de los usuarios, y su sueldo. Debes considerar un json de ejmplo con los datos, en el que se registra para cada usuario su dni, año de nacimiento, fecha de ingreso en la empresa, plus de productividad en %, plus de antigüedad bruto (0,1% por cada tres años trabajado) y el sueldo base. (El json debe estar en el propio código, inicializando una variable, explica por qué se te exige este requisito)
- */
-function ageAndSalary() {
-    let userdataJSON = {
-        "name": "Miguel",
-        "dni": 11112222,
-        "birthYear": 1986,
-        "startDate": "12-06-2015",
-        "productivityPlus": 10,
-        "baseAmount": 1600,
+document.getElementById('addTaskForm').addEventListener('submit', saveTask);
+document.getElementById('resetButton').addEventListener('click', resetTable);
+document.getElementById('resetButton').addEventListener('onkeypress', resetTable);
+printTasks();
+function initializeTable(e) {
+    if(localStorage.getItem('taskList') === null) {
+        let taskList = [["07:00 - 08:00", " ", " ", " ", " ", " "], ["08:00 - 09:00", " ", " ", " ", " ", " "], ["09:00 - 10:00", " ", " ", " ", " ", " "], ["10:00 - 11:00", " ", " ", " ", " ", " "], ["11:00 - 12:00", " ", " ", " ", " ", " "], ["12:00 - 13:00", " ", " ", " ", " ", " "], ["13:00 - 14:00", " ", " ", " ", " ", " "]];
+        localStorage.setItem('taskList', JSON.stringify(taskList));
     }
-    let currentYear = new Date().getFullYear();
-    let age = currentYear - userdataJSON['birthYear'];
-    let yearInTheCompany = Math.trunc((new Date() - new Date(userdataJSON['startDate'])) / 1000 / 60 / 60 / 24 / 365);
-    let fullSalary = userdataJSON['baseAmount'] * (1 + userdataJSON['productivityPlus'] / 100) * (1 + 0.001 * (Math.trunc(yearInTheCompany / 3)));
-    console.log(`El empleado ${userdataJSON['name']}, con DNI ${userdataJSON['dni']} y ${age} años, tendrá este mes:
-                     Sueldo total:  ${fullSalary}€ `)
 }
 
+function resetTable(e) {
+    let taskList = [["07:00 - 08:00", " ", " ", " ", " ", " "], ["08:00 - 09:00", " ", " ", " ", " ", " "], ["09:00 - 10:00", " ", " ", " ", " ", " "], ["10:00 - 11:00", " ", " ", " ", " ", " "], ["11:00 - 12:00", " ", " ", " ", " ", " "], ["12:00 - 13:00", " ", " ", " ", " ", " "], ["13:00 - 14:00", " ", " ", " ", " ", " "]];
+    localStorage.setItem('taskList', JSON.stringify(taskList));
+    printTasks();
+}
 
-/**
- * Crear un script que muestre un informe de la cantidad de párrafos, enlaces y div en tu página de inicio.
- */
-function generarInforme() {
-    let parrafos_sz = 0, link_sz = 0, div_sz = 0;
-    parrafos_sz = $('p').length;
-    link_sz = $('a').length;
-    div_sz = $('div').length;
-    console.log(`Se ha analizado todo el documento. En su interior, se ha encontrado la siguiente cantidad de elementos:
-                        Párrafos:           ${parrafos_sz}.
-                        Enlaces:            ${link_sz}.
-                        Contenedores div:   ${div_sz}.`)
+function saveTask(e) {
+    console.log("called");
+    initializeTable();
+    let taskTitle = document.getElementById('taskTitle').value;
+    let taskDay  = document.getElementById('taskDay').value;
+    let taskHour  = document.getElementById('taskHour').value;
+    const task = {
+        title: taskTitle,
+        day: taskDay,
+        hour: taskHour
+    }
+    console.log(task);
+    let taskList = JSON.parse(localStorage.getItem('taskList'));
+    taskList[task.hour][task.day] = task.title;
+    localStorage.setItem('taskList', JSON.stringify(taskList));
+    printTasks();
+    e.preventDefault();
+}
+
+function printTasks() {
+    let taskList = JSON.parse(localStorage.getItem('taskList'));
+    let taskDiv = document.getElementById('schedule');
+    let tmpdiv = "";
+    tmpdiv = `<br><br><table class="striped center">
+    <thead>
+        <tr class="white-text">
+            <td>Horario</td>
+            <td>Lunes</td>
+            <td>Martes</td>
+            <td>Miércoles</td>
+            <td>Jueves</td>
+            <td>Viernes</td>
+        </tr>
+    </thead>
+
+    <tbody>`;
+
+    let aux = true;
+
+    for (let i = 0; i < 7; i++) {
+        tmpdiv += `<tr class=${aux ? " " : "white-text"}>`;
+        aux = !aux;
+        for (let j = 0; j < 6; j++) {
+            const task = taskList[i][j];
+            tmpdiv += `<td>${task}</td>`;
+        }
+        tmpdiv +='</tr>';
+    }
+    tmpdiv +='</tbody></table>';
+    console.log(tmpdiv);
+    taskDiv.innerHTML = tmpdiv;
 }
